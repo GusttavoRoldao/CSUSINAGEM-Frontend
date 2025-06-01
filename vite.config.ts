0afd2564ef50e31fbee6285cdc40037417985b57
Lucas Cordeiro <Lucas_tb1315@hotmail.com>
2025-06-01 18:38:46 +0000

    att

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/customers': 'http://localhost:3333',
-      '/customer': 'http://localhost:3333'
+      '/customer': 'http://localhost:3333',
     }
   }
 })
